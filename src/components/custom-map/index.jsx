4aaa583c4c7beed2c6c58a9f0da26ee9e5450b7b
Mac <jon_markov@sina.com>
2025-04-14 19:35:12 +0800

    样式

--- src/components/custom-map/index.jsx
+++ src/components/custom-map/index.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
-import { View } from '@tarojs/components'
+import { View, Text, Image } from '@tarojs/components';
+import { returnCar, seekCar, unlock, closelock, origin, goNav, bluetooth, wifi, bluetooth_1, wifi_1 } from '@utils/image-out';
 import Taro from '@tarojs/taro'
+import './index.less';
 
 export default function MapWithPopup() {
     const [activeInfoWindow, setActiveInfoWindow] = useState(null)
@@ -82,18 +84,59 @@ export default function MapWithPopup() {
     }, [])
 
     return (
-        <View
-            id="map-container"
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                margin: "auto",
-                width: '96%',
-                height: '100%',
-                borderRadius: '8px',
-                overflow: 'hidden'
-            }}
-        />
+        <View className='main-container'>
+            <View
+                id="map-container"
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    margin: "auto",
+                    width: '96%',
+                    height: '100%',
+                    borderRadius: '8px',
+                    overflow: 'hidden'
+                }}
+            />
+            <View className='map-tools' style={{ bottom: `10px` }}>
+                <View className='map-tools-container'>
+                    <Image src={returnCar} className='map-tools-image' />
+                    <Text className='map-tools-text'>归还车辆</Text>
+                </View>
+                <View className='map-tools-container'>
+                    <Image src={unlock} className='map-tools-image' />
+                    <Text className='map-tools-text'>开锁</Text>
+                </View>
+                <View className='map-tools-container'>
+                    <Image src={closelock} className='map-tools-image' />
+                    <Text className='map-tools-text'>关锁</Text>
+                </View>
+                <View className='map-tools-container'>
+                    <Image src={seekCar} className='map-tools-image' />
+                    <Text className='map-tools-text'>寻车</Text>
+                </View>
+                <View className='map-tools-container'>
+                    <Image src={seekCar} className='map-tools-image' />
+                    <Text className='map-tools-text'>查看照片</Text>
+                </View>
+            </View>
+            <View className='map-nav' style={{ bottom: `50px`, right: '5%' }}>
+                <View className='map-nav-container'>
+                    <Image src={origin} className='map-nav-image' />
+                </View>
+                <View className='map-nav-container'>
+                    <Image src={goNav} className='map-nav-image' />
+                </View>
+            </View>
+            <View className='map-network' style={{ top: `10px`, right: `90%` }}>
+                <View className='map-network-container'>
+                    <Image src={bluetooth} className='map-network-image' />
+                </View>
+                <View className='map-network-container'>
+                    <Image src={wifi_1} className='map-network-image' />
+                </View>
+            </View>
+        </View>
+
     )
 }
